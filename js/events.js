9751cd338ce034d657cca019a0b561f044833baf
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-02-20 20:19:28 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -5,3 +5,24 @@ $(document).ready(function(){
 // call functions here
 
 });
+
+function getIt() {
+  $('p').addEventListener("click", function() {
+    alert("Hey!") ;
+  })
+}
+function frameIt() {
+  $(img).addEventListener("load", function() {
+    self.class = "tasty"
+  })
+}
+function pressIt() {
+  $('#typing').addEventListener("keydown", function(e) {
+    if (e.which === 71) {alert('You have pressed the G key')}
+  })
+}
+function submitIt() {
+  $('form').addEventListener("submit", function() {
+    alert('Your form is going to be submitted now.')
+  })
+}
